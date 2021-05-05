922fa7efe4fcc075e491198f9bf34b07222913de
BhupeshVerma <bhupesh.v94@gmail.com>
2021-05-06 02:46:33 +0530

    Displaying posts

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Card, Button, Image, Icon, Label } from 'semantic-ui-react';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
+
+const PostCard = ({
+  post: {
+    body,
+    createdAt,
+    username,
+    id,
+    likeCount,
+    commentCount,
+    likes,
+    comments,
+  },
+}) => {
+    const likePost = () => {
+        console.log('Like post!!');
+    }
+    const commentOnPost = () => {
+      console.log("comment on  post!!");
+    };
+    return (
+      <Card fluid>
+        <Card.Content>
+          <Image
+            floated="right"
+            size="mini"
+            src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+          />
+          <Card.Header>{username}</Card.Header>
+          <Card.Meta as={Link} to={`/posts/${id}`}>
+            {moment(createdAt).fromNow(true)}
+          </Card.Meta>
+          <Card.Description>{body}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <Button as="div" labelPosition="right" onClick={likePost}>
+            <Button basic color="teal">
+              <Icon name="heart" />
+            </Button>
+            <Label basic color="teal" pointing="left">
+              {likeCount}
+            </Label>
+          </Button>
+          <Button as="div" labelPosition="right" onClick={commentOnPost}>
+            <Button basic color="blue">
+              <Icon name="comments" />
+            </Button>
+            <Label basic color="blue" pointing="left">
+              {commentCount}
+            </Label>
+          </Button>
+        </Card.Content>
+      </Card>
+    );
+};
+
+export default PostCard
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,59 @@
 import React from 'react'
+import { gql, useQuery } from "@apollo/client";
+import { Grid } from 'semantic-ui-react';
+import PostCard from '../components/PostCard';
+// import gql from "graphql-tag";
 
-function Home() {
+const Home = () => {
+    const { loading, data: { getPosts: posts } } = useQuery(FETCH_POSTS_QUERY);
+    if (posts) {
+        console.log(posts);
+        
+    }
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    )
-}
+      <Grid columns={3}>
+        <Grid.Row className="page-title">
+          <h1>Recent posts</h1>
+        </Grid.Row>
+        <Grid.Row>
+          {loading ? (
+            <h1>Loading...</h1>
+          ) : posts ? (
+            posts.map((post) => (
+              <Grid.Column key={post.id} style={{marginBottom: 20}}>
+                <PostCard post={post} />
+              </Grid.Column>
+            ))
+          ) : (
+            ""
+          )}
+        </Grid.Row>
+      </Grid>
+    );
+};
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts{
+      id
+      body
+      username
+      createdAt
+      comments {
+        id
+        username
+        body
+        createdAt
+      }
+      likes {
+        id
+        username
+        createdAt
+      }
+      commentCount
+      likeCount
+    }
+  }
+`;
 
 export default Home
